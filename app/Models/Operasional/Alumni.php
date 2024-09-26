b81060d3cd64797b2d3b4e579a0f5979f12b30dc
lennafebriana12 <lennahutapea18@gmail.com>
2024-09-26 16:21:16 +0700

    add model

---
+++ app/Models/Operasional/Alumni.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Operasional;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Alumni extends Model
+{
+    use HasFactory;
+}
