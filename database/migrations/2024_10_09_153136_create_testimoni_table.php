b5f26d534339279a4b123e67a49366176faa3358
mulyadiyaminsiahaan <112603524+mulyadiyaminsiahaan@users.noreply.github.com>
2024-12-02 14:10:33 +0700

    database

--- database/migrations/2024_10_09_153136_create_testimoni_table.php
+++ database/migrations/2024_10_09_153136_create_testimoni_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('testimoni', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->nullable();
+            $table->foreignId('user_id')->nullable(); 
             $table->string('pekerjaan');
             $table->string('program_studi');
             $table->string('angkatan');
